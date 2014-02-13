be536b0cc4e1c3d4241753e0c2f09cf637fcb5ed
karenyng <kyyng@ucdavis.edu>
2014-02-12 17:29:27 -0800

    latest version of method2.R that admits user arguments to specify # of files used

--- method2.R
+++ method2.R
@@ -6,18 +6,23 @@
 require(AirlineDelays)
 
 # this numCore variable should be input from the console instead 
+numCore <- as.integer(commandArgs(trailingOnly = T))
+print(paste("numCore =", numCore))
+if(!(is.integer(numCore) && numCore > 0))
+  stop("Wrong numCore commandline argument, not integer!")
 
 # ----Initialize some variables ----- 
-#filepath <- "/mnt/Winter14Stat250/HW2_Stat250_W14/data"
-filepath <- "/mnt/Winter14Stat250/HW2_Stat250_W14/tests"
+filepath <- "/mnt/Winter14Stat250/HW2_Stat250_W14/data"
+#filepath <- "/mnt/Winter14Stat250/HW2_Stat250_W14/tests"
 #pattern <- "^([0-9]+).csv$"
 #pattern <- "^2008([a-zA-Z_]+).csv$"
 #pattern <- "^198[7-9].csv$"
-#pattern <- "^([0-9]+)([a-zA-Z_]+)?.csv$"
-pattern <- "^test1.csv$"
+pattern <- "^([0-9]+)([a-zA-Z_]+)?.csv$"
+#pattern <- "^test1.csv$"
 FILES <- getListOfFiles(filepath, pattern = pattern)
-print(paste("Setting numThreads = number of files input =", length(FILES)))
-tt <- getDelayTable_thread(FILES, numThreads = length(FILES))
+print(paste("Setting numThreads = number of files input =",
+            length(FILES[c(1:numCore)])))
+tt <- getDelayTable_thread(FILES[c(1:numCore)], numThreads = numCore)
 ans <- freq_mean(tt)
 std <- freq_sd(ans[[2]], tt, ans[[1]])
 
